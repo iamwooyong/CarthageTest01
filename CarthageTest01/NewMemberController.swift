09594baf11946db64dd7c1c7510f6b3b51f8e82b
imwy <iamwooyong@gmail.com>
2019-05-22 19:11:07 +0900

    회원가입 개발 중

---
+++ CarthageTest01/NewMemberController.swift
@@ -0,0 +1,30 @@
+//
+//  NewMemberController.swift
+//  CarthageTest01
+//
+//  Created by NICE on 22/05/2019.
+//  Copyright © 2019 NICE. All rights reserved.
+//
+
+import UIKit
+
+class NewMemberController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
